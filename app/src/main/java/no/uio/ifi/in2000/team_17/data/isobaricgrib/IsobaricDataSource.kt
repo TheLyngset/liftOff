76cbfa66900ca11684cbca965534be9bc7bfdf8b
amand <amandusguldahl@gmail.com>
2024-03-11 09:40:15 +0100

    Change functions and some variable names to better represent best practices.

--- app/src/main/java/no/uio/ifi/in2000/team_17/data/isobaricgrib/IsobaricDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/data/isobaricgrib/IsobaricDataSource.kt
@@ -6,27 +6,26 @@ import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.serialization.gson.gson
-import java.nio.channels.UnresolvedAddressException
-import com.google.gson.annotations.SerializedName
 import no.uio.ifi.in2000.team_17.data.isobaricgrib.model.IsoBaricModel
+import java.nio.channels.UnresolvedAddressException
 
 
 val LOG_NAME = "ISOBARICDATASOURCE"
-class IsobaricDataSource{
-    val client = HttpClient { install(ContentNegotiation) { gson() } }
+private const val BASE_URL = "http://20.100.26.176:5000/collections/isobaric/position"
 
-    val BASE_URL = "http://20.100.26.176:5000/collections/isobaric/position"
-    fun makeQueryUrl(north: Double,east: Double ): String {
-        val queryString = "$BASE_URL?coords=POINT%28$east%20$north%29"
-        // Log.v(logName, "queryUrl is $queryString")
-        return queryString
+class IsobaricDataSource {
+
+    private val client = HttpClient { install(ContentNegotiation) { gson() } }
+    fun makeQueryUrl(north: Double, east: Double): String {
+        return "$BASE_URL?coords=POINT%28$east%20$north%29"
     }
-    suspend fun getData(north:Double, east:Double): IsoBaricModel {
+
+    suspend fun getData(north: Double, east: Double): IsoBaricModel {
         return try {
             Log.d(LOG_NAME, makeQueryUrl(north, east))
             client.get(makeQueryUrl(north, east)).body<IsoBaricModel>()
-        }catch (e: UnresolvedAddressException){
-            Log.d(LOG_NAME, "Unresolved adress")
+        } catch (e: UnresolvedAddressException) {
+            Log.d(LOG_NAME, "Unresolved address")
             IsoBaricModel()
         }
     }
--- app/src/main/java/no/uio/ifi/in2000/team_17/data/isobaricgrib/IsobaricRepo.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/data/isobaricgrib/IsobaricRepo.kt
@@ -1,10 +1,6 @@
 package no.uio.ifi.in2000.team_17.data.isobaricgrib
 
-import android.util.Log
-import androidx.compose.runtime.mutableStateListOf
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import no.uio.ifi.in2000.team_17.data.isobaricgrib.model.IsoBaricModel
 import kotlin.math.PI
@@ -15,26 +11,27 @@ import kotlin.math.round
 import kotlin.math.sin
 import kotlin.math.sqrt
 
-val GRAVITATIONAL_ACCELERATION: Double = 9.834 // m/s^2
-val GAS_CONSTANT_AT_DRY_AIR: Double = 287.052874 // J⋅kg−1⋅K−1
+const val GRAVITATIONAL_ACCELERATION: Double = 9.834 // m/s^2
+const val GAS_CONSTANT_AT_DRY_AIR: Double = 287.052874 // J⋅kg−1⋅K−1
 
-data class SealevelData(
-    val windSpeed:Double = 0.0,
-    val windDirection:Double = 0.0,
-    val pressure:Double = 1000.0,
+data class SeaLevelData(
+    val windSpeed: Double = 0.0,
+    val windDirection: Double = 0.0,
+    val pressure: Double = 1000.0,
     val temperature: Double = 0.0
 )
+
 class IsobaricRepo {
     private val dataSource = IsobaricDataSource()
-    private var pressureAtSeaLevel:Double = 1000.0
+    private var pressureAtSeaLevel: Double = 1000.0
     private val isoBaricModel = MutableStateFlow(IsoBaricModel())
     val layerHeights = MutableStateFlow<List<Double>>(listOf())
     val windSpeeds = MutableStateFlow<List<Double>>(listOf())
     val windFromDirection = MutableStateFlow<List<Double>>(listOf())
     val windShear = MutableStateFlow<List<Double>>(listOf())
     val temperatures = MutableStateFlow<List<Double>>(listOf())
-    suspend fun loadData(north:Double, east:Double, sealevelData:SealevelData = SealevelData()){
-        isoBaricModel.update {dataSource.getData(north, east)}
+    suspend fun loadData(north: Double, east: Double, sealevelData: SeaLevelData = SeaLevelData()) {
+        isoBaricModel.update { dataSource.getData(north, east) }
 
         val pressures = isoBaricModel.value.domain.axes.z.values
         isoBaricModel
@@ -52,14 +49,15 @@ class IsobaricRepo {
         var s_0 = sealevelData.windSpeed
         var d_0 = sealevelData.windDirection
         windShear.update { listOf() }
-        windSpeeds.value.zip(windFromDirection.value).forEach {(s_1, d_1)->
-            windShear.update { it + listOf<Double>(round(WindShear(s_0, d_0, s_1, d_1))) }
+        windSpeeds.value.zip(windFromDirection.value).forEach { (s_1, d_1) ->
+            windShear.update { it + listOf<Double>(round(windShear(s_0, d_0, s_1, d_1))) }
             s_0 = s_1
             d_0 = d_1
         }
 
     }
-    fun hydrostaticFormula(
+
+    private fun hydrostaticFormula(
         pressure: Double, temperature: Double, pressureAtSeaLevel: Double
     ): Double {
         val tempInKelvin = temperature + 273.15
@@ -67,9 +65,14 @@ class IsobaricRepo {
         return round((GAS_CONSTANT_AT_DRY_AIR / GRAVITATIONAL_ACCELERATION) * tempInKelvin * ln((pressureAtSeaLevel / pressure))) //TODO: Check if this is right
 
     }
-    fun WindShear(s_0:Double, d_0: Double, s_1:Double, d_1: Double): Double{
+
+    private fun windShear(s_0: Double, d_0: Double, s_1: Double, d_1: Double): Double {
         val d_0_rad = d_0 * PI / 180
         val d_1_rad = d_1 * PI / 180
-        return sqrt((s_1*cos(d_1_rad) - s_0*cos(d_0_rad)).pow(2) + (s_1*sin(d_1_rad) - s_0*sin(d_0_rad)).pow(2))
+        return sqrt(
+            (s_1 * cos(d_1_rad) - s_0 * cos(d_0_rad)).pow(2) + (s_1 * sin(d_1_rad) - s_0 * sin(
+                d_0_rad
+            )).pow(2)
+        )
     }
 }
