f99f4103b6fb95e040f4c405a9fc40b682fc71a8
Aguldahl <amandusguldahl@gmail.com>
2024-03-13 10:40:44 +0100

    Introduce LOG_NAME variable to LocationForecastRepository for easier re-use and less boilerplate

--- app/src/main/java/no/uio/ifi/in2000/team_17/data/locationforecast/LocationForecastRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/data/locationforecast/LocationForecastRepository.kt
@@ -6,24 +6,26 @@ import no.uio.ifi.in2000.team_17.data.locationforecast.weatherDTO.Locationforeca
 import java.io.IOException
 import kotlin.math.round
 
-interface LocationForecastRepository{
-   // suspend fun getLocationforecastData(lat: Double, lon: Double): LocationforecastDTO
-    suspend fun getGroundWeatherPoint(lan: Double, lon: Double, index: Int): WeatherPoint
+interface LocationForecastRepository {
+    // suspend fun getLocationforecastData(lat: Double, lon: Double): LocationforecastDTO
+    suspend fun getGroundWeatherPoint(lat: Double, lon: Double, index: Int): WeatherPoint
 }
-class LocationForecastRepositoryImplementation (
+
+class LocationForecastRepositoryImplementation(
     private val locationforecastDataSource: LocationForecastDataSource = LocationForecastDataSource()
-) : LocationForecastRepository  {
+) : LocationForecastRepository {
+    private val LOG_NAME = "LOCATIONFORECAST_REPOSITORY"
     suspend fun getLocationforecastData(lat: Double, lon: Double): LocationforecastDTO {
         var locationforecastData = LocationforecastDTO(null, null, null)
         try {
             locationforecastData = locationforecastDataSource.fetchLocationforecast(lat, lon)
         } catch (e: IOException) {
             // log the exception
-            Log.e("LOCATIONFORECAST_REPOSITORY", "Error while fetching Locationforecast data: ${e.message}")
+            Log.e(LOG_NAME, "Error while fetching Locationforecast data: ${e.message}")
         } catch (e: Exception) {
-            Log.e("LOCATIONFORECAST_REPOSITORY", "Error while fetching Locationforecast data: ${e.message}")
+            Log.e(LOG_NAME, "Error while fetching Locationforecast data: ${e.message}")
         }
-        //Log.d("REPOSITORY", "type: ${locationforecastData.type}")
+        //Log.d(LOG_NAME, "type: ${locationforecastData.type}")
 
         return locationforecastData
     }
@@ -33,32 +35,39 @@ class LocationForecastRepositoryImplementation (
     //rename this to ground weather point
     override suspend fun getGroundWeatherPoint(lat: Double, lon: Double, index: Int): WeatherPoint {
         val allLocationData = getLocationforecastData(lat, lon)
-        val windSpeed: Double? = allLocationData.properties?.timeseries?.getOrNull(index)?.data?.instant?.details?.wind_speed
-        val windFromDirection: Double? = allLocationData.properties?.timeseries?.getOrNull(index)?.data?.instant?.details?.wind_from_direction
-        val airTemperature: Double? = allLocationData.properties?.timeseries?.getOrNull(index)?.data?.instant?.details?.air_temperature
-        val pressureSeaLevel: Double? = allLocationData.properties?.timeseries?.getOrNull(index)?.data?.instant?.details?.air_pressure_at_sea_level
-        val cloudFraction: Double? = allLocationData.properties?.timeseries?.getOrNull(index)?.data?.instant?.details?.cloud_area_fraction
-        val rain: Double? = allLocationData.properties?.timeseries?.getOrNull(index)?.data?.next_1_hours?.details?.precipitation_amount
-        val relativeHumidity: Double? = allLocationData.properties?.timeseries?.getOrNull(index)?.data?.instant?.details?.relative_humidity
-        val dewPoint: Double? = computeDewPointGround(airTemperature, relativeHumidity)
+        val windSpeed: Double? =
+            allLocationData.properties?.timeseries?.getOrNull(index)?.data?.instant?.details?.wind_speed
+        val windFromDirection: Double? =
+            allLocationData.properties?.timeseries?.getOrNull(index)?.data?.instant?.details?.wind_from_direction
+        val airTemperature: Double? =
+            allLocationData.properties?.timeseries?.getOrNull(index)?.data?.instant?.details?.air_temperature
+        val pressureSeaLevel: Double? =
+            allLocationData.properties?.timeseries?.getOrNull(index)?.data?.instant?.details?.air_pressure_at_sea_level
+        val cloudFraction: Double? =
+            allLocationData.properties?.timeseries?.getOrNull(index)?.data?.instant?.details?.cloud_area_fraction
+        val rain: Double? =
+            allLocationData.properties?.timeseries?.getOrNull(index)?.data?.next_1_hours?.details?.precipitation_amount
+        val relativeHumidity: Double? =
+            allLocationData.properties?.timeseries?.getOrNull(index)?.data?.instant?.details?.relative_humidity
+        val dewPoint: Double = computeDewPointGround(airTemperature, relativeHumidity)
 
         //var weatherPoint = GroundWeatherPoint(windSpeed, windFromDirection, airTemperature, pressureSeaLevel,cloudFraction,rain, humidity, 0.0)
         val weatherPoint = WeatherPoint(
-            windSpeed= windSpeed!!,
-            windDirection =  windFromDirection!!,
-            temperature =  airTemperature!!,
+            windSpeed = windSpeed!!,
+            windDirection = windFromDirection!!,
+            temperature = airTemperature!!,
             pressure = pressureSeaLevel!!,
-            cloudFraction =  cloudFraction!!,
+            cloudFraction = cloudFraction!!,
             rain = rain!!,
             humidity = relativeHumidity!!,
             height = 0.0,
-            dewPoint = dewPoint!!
+            dewPoint = dewPoint
         )
 
         return weatherPoint
     }
 
-    private fun computeDewPointGround(temperature: Double?, relativeHumidity: Double?): Double?{
+    private fun computeDewPointGround(temperature: Double?, relativeHumidity: Double?): Double {
         //https://iridl.ldeo.columbia.edu/dochelp/QA/Basic/dewpoint.html
         //Td = T - ((100 - RH)/5.)
         //Td is dew point temperature (in degrees Celsius),
@@ -66,9 +75,9 @@ class LocationForecastRepositoryImplementation (
         // RH is relative humidity (in percent).
         // Apparently this relationship is fairly accurate for relative humidity values above 50%.
         if (temperature != null && relativeHumidity != null) {
-            return round((temperature!! - ((100- relativeHumidity)/5)))
+            return round((temperature - ((100 - relativeHumidity) / 5)))
         }
         return -1.0
     }
-    
+
 }
