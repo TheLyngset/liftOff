1aa173b2d3ac84fe836163f1bb41d1494d39e8a1
Aguldahl <amandusguldahl@gmail.com>
2024-03-14 10:10:51 +0100

    Add white space, for better readability

--- app/src/main/java/no/uio/ifi/in2000/team_17/data/Repository.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/data/Repository.kt
@@ -20,10 +20,13 @@ import kotlin.math.sqrt
 
 const val GRAVITATIONAL_ACCELERATION: Double = 9.834 // m/s^2
 const val GAS_CONSTANT_AT_DRY_AIR: Double = 287.052874 // J⋅kg−1⋅K−1
+
 class Repository {
+    private val LOG_NAME = "REPOSITORY"
+
+    // Load data from isoBaricDataSource and locationForecast,
     private val isoBaricDataSource = IsobaricDataSource()
     private val locationForecastDataSource = LocationForecastDataSource()
-    private val LOG_NAME = "REPOSITORY"
     private var pressureAtSeaLevel: Double = 1000.0
 
     private val isoBaricData = MutableStateFlow(IsoBaricModel())
@@ -31,19 +34,20 @@ class Repository {
     val weatherPointList = MutableStateFlow<List<WeatherPoint>>(listOf())
 
 
-    suspend fun load(latLng: LatLng, maxHeight: Int){
+    suspend fun load(latLng: LatLng, maxHeight: Int) {
         loadLocationForecast(latLng)
         loadIsobaricData(latLng, maxHeight, generateGroundWeatherPoint())
 
     }
-    private suspend fun loadLocationForecast(latLng: LatLng){
+
+    private suspend fun loadLocationForecast(latLng: LatLng) {
         locationForecastData.update {
-            try{
+            try {
                 locationForecastDataSource.fetchLocationforecast(
                     round(latLng.latitude),
                     round(latLng.longitude)
                 )
-            }catch (e: IOException) {
+            } catch (e: IOException) {
                 Log.e(LOG_NAME, "Error while fetching Locationforecast data: ${e.message}")
                 LocationforecastDTO(null, null, null)
             } catch (e: Exception) {
@@ -53,7 +57,11 @@ class Repository {
         }
     }
 
-    private suspend fun loadIsobaricData(latLng: LatLng, maxHeight: Int, groundWeatherPoint:WeatherPoint){
+    private suspend fun loadIsobaricData(
+        latLng: LatLng,
+        maxHeight: Int,
+        groundWeatherPoint: WeatherPoint
+    ) {
         pressureAtSeaLevel = groundWeatherPoint.pressure
         isoBaricData.update { isoBaricDataSource.getData(latLng.latitude, latLng.longitude) }
         val layerHeights = mutableListOf<Double>()
@@ -70,16 +78,17 @@ class Repository {
         val temperatures = isoBaricData.value.ranges.temperature.values
         val windFromDirection = isoBaricData.value.ranges.windFromDirection.values
         val windShear = mutableListOf<Double>()
-        windSpeed.zip(windFromDirection).forEach { (s_1, d_1) -> //Wind speed and wind direction at higher level
-            windShear.add(round(calculateWindShear(s_0, d_0, s_1, d_1)))
-            s_0 = s_1
-            d_0 = d_1
-        }
+        windSpeed.zip(windFromDirection)
+            .forEach { (s_1, d_1) -> //Wind speed and wind direction at higher level
+                windShear.add(round(calculateWindShear(s_0, d_0, s_1, d_1)))
+                s_0 = s_1
+                d_0 = d_1
+            }
         weatherPointList.update {
             val allLayers = listOf(groundWeatherPoint) + windSpeed.zip(windFromDirection)
                 .zip(temperatures) { (speed, direction), temperature ->
                     Triple(speed, direction, temperature)
-                }.zip(windShear){(speed, direction, temperature), shear ->
+                }.zip(windShear) { (speed, direction, temperature), shear ->
                     listOf(speed, direction, temperature, shear)
                 }
                 .zip(pressures) { (speed, direction, temperature, shear), pressure ->
@@ -92,7 +101,7 @@ class Repository {
                         height = calculateHeight(pressure, temperature, pressureAtSeaLevel)
                     )
                 }
-            allLayers.filter { it.height <= maxHeight*1000 + 1000 }
+            allLayers.filter { it.height <= maxHeight * 1000 + 1000 }
         }
     }
 
@@ -129,7 +138,6 @@ class Repository {
             height = 0.0,
             dewPoint = dewPoint
         )
-
         return weatherPoint
     }
 
@@ -149,6 +157,7 @@ class Repository {
             )).pow(2)
         )
     }
+
     private fun computeDewPointGround(temperature: Double?, relativeHumidity: Double?): Double {
         //https://iridl.ldeo.columbia.edu/dochelp/QA/Basic/dewpoint.html
         //Td = T - ((100 - RH)/5.)
