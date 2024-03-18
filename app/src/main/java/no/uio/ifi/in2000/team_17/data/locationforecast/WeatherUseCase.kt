48e238ea0d16ba2d9cefe037bf19704ba7023581
Lelia M <leliamm@ulrik.uio.no>
2024-03-18 18:29:54 +0100

    updaded LaunchClearanceUnitTest to incorporate values and test for Fog.

--- app/src/main/java/no/uio/ifi/in2000/team_17/data/locationforecast/WeatherUseCase.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/data/locationforecast/WeatherUseCase.kt
@@ -45,6 +45,7 @@ internal fun launchClearance(
                     weatherPoint.cloudFraction < 15.0 &&
                     weatherPoint.rain < 0.1 &&
                     weatherPoint.windShear < 24.5 &&
+                    weatherPoint.fog < 0.1 &&
                     maxWindSpeed < 17.2 &&
                     maxShearWind < 24.5)
 }
