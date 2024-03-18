2c48eac2535138fa7db84e4b85a7a2d7f03bbf3b
Lelia M <leliamm@ulrik.uio.no>
2024-03-18 10:52:30 +0100

    deleted unnecessary comments form tests.

--- app/src/test/java/no/uio/ifi/in2000/team_17/dataLayerUnitTests/DewPointUnitTest.kt
+++ app/src/test/java/no/uio/ifi/in2000/team_17/dataLayerUnitTests/DewPointUnitTest.kt
@@ -6,32 +6,29 @@ import org.junit.Test
 
 class DewPointUnitTest {
     @Test
-    fun dewPoint_isCorrectHUMover50(){
+    fun dewPoint_isCorrectHUMover50() {
         //arrange and act
         val result = computeDewPointGround(10.0, 100.0)
         //assert
-        //assertEquals(10.0 , result)
         assertTrue(result in 9.0..11.0);
     }
 
     @Test
-    fun dewPoint_isCorrectNegTemp(){
+    fun dewPoint_isCorrectNegTemp() {
         //arrange and act
         val result = computeDewPointGround(-10.0, 60.0)
         //assert
-        //assertEquals(-18.0 , result)
         assertTrue(result in -19.0..-17.0);
 
     }
+
     @Test
-    fun dewPoint_isCorrectPosTemp(){
+    fun dewPoint_isCorrectPosTemp() {
         //arrange and act
         val result = computeDewPointGround(15.0, 60.0)
         //assert
-       // assertEquals(7.29 , result) -- +- 1 degree
         assertTrue(result in 6.29..8.29);
     }
-
 
 
 }
--- app/src/test/java/no/uio/ifi/in2000/team_17/dataLayerUnitTests/HydrostaticFormulaUnitTests.kt
+++ app/src/test/java/no/uio/ifi/in2000/team_17/dataLayerUnitTests/HydrostaticFormulaUnitTests.kt
@@ -1,17 +1,14 @@
 package no.uio.ifi.in2000.team_17.dataLayerUnitTests
 
-import junit.framework.TestCase
-import junit.framework.TestCase.assertTrue
 import no.uio.ifi.in2000.team_17.data.calculateHeight
 import org.junit.Test
 
 class HydrostaticFormulaUnitTests {
     @Test
-    fun heigth_isCorrect(){
+    fun heigth_isCorrect() {
         //arrange and act
         val result = calculateHeight(10.0, -10.0, 101.32)
         //assert
-        //assertEquals(10.0 , result)
         //assertTrue(result in 9.0..11.0)
     }
 }
