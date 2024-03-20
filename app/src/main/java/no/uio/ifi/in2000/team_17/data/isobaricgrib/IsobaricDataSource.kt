3ec8ca04a6f46fe6661948d05541251a33faaeb7
Aguldahl <amandusguldahl@gmail.com>
2024-03-20 12:45:32 +0100

    Remove makeQueryUrl and add url to getData

--- app/src/main/java/no/uio/ifi/in2000/team_17/data/isobaricgrib/IsobaricDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/data/isobaricgrib/IsobaricDataSource.kt
@@ -7,7 +7,6 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.serialization.gson.gson
 import no.uio.ifi.in2000.team_17.data.isobaricgrib.model.IsoBaricModel
-import java.io.IOException
 
 
 /**
@@ -25,18 +24,14 @@ class IsobaricDataSource {
     private val BASE_URL = "http://4.235.14.248:5000/collections/isobaric/position"
 
     private val client = HttpClient { install(ContentNegotiation) { gson() } }
-    private fun makeQueryUrl(north: Double, east: Double): String {
-        return "$BASE_URL?coords=POINT%28$east%20$north%29"
-    }
-
 
     // Returns empty IsoBaricModel object on fail
     suspend fun getData(north: Double, east: Double): IsoBaricModel {
-        val queryUrl: String = makeQueryUrl(north, east)
+        val queryUrl = "$BASE_URL?coords=POINT%28$east%20$north%29"
         return try {
             Log.d(LOG_NAME, "Attempting to fetch data from: $queryUrl")
             client.get(queryUrl).body<IsoBaricModel>()
-        } catch (e: IOException) {
+        } catch (e: Exception) {
             Log.e(LOG_NAME, "Got an IO exception while trying to fetch from $queryUrl", e)
             IsoBaricModel()
         }
