ccc897f7df04a490b6c686e2300e0fe6984b285b
Lelia M <leliamm@ulrik.uio.no>
2024-04-03 11:36:58 +0200

    changed all default values of the weather point to -1.0

--- app/src/main/java/no/uio/ifi/in2000/team_17/model/WeatherPoint.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/model/WeatherPoint.kt
@@ -3,12 +3,12 @@ package no.uio.ifi.in2000.team_17.model
 
 // May be used instead of keeping up with a multitude of lists and their indexes
 data class WeatherPoint(
-    val windSpeed: Double = 0.0,
-    val windDirection: Double = 0.0,
-    val windShear: Double = 0.0,
-    val temperature: Double = 0.0,
-    val pressure: Double = 1000.00,
-    val height: Double = 0.0,
+    val windSpeed: Double = -1.0,
+    val windDirection: Double = -1.0,
+    val windShear: Double = -1.0,
+    val temperature: Double = -1.0,
+    val pressure: Double = -1.0,
+    val height: Double = -1.0,
     val cloudFraction: Double = -1.0,
     val rain: Double = -1.0,
     val humidity: Double = -1.0,
