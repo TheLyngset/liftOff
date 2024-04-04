73f8dddb0ade1ff89da4b0e7fa06cf59be1777a3
Lelia M <leliamm@ulrik.uio.no>
2024-04-04 09:59:54 +0200

    updated repository to be used together with the FututreWeatherUseCase.kt

--- app/src/main/java/no/uio/ifi/in2000/team_17/data/locationforecast/LocationForecastRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/data/locationforecast/LocationForecastRepository.kt
@@ -1,5 +1,6 @@
 package no.uio.ifi.in2000.team_17.data.locationforecast
 
+import android.icu.util.Calendar
 import android.util.Log
 import no.uio.ifi.in2000.team_17.model.WeatherPoint
 import no.uio.ifi.in2000.team_17.model.weatherDTO.Details
@@ -8,14 +9,14 @@ import java.io.IOException
 import kotlin.math.round
 
 interface LocationForecastRepository {
-    // suspend fun getLocationforecastData(lat: Double, lon: Double): LocationforecastDTO
-    suspend fun getGroundWeatherPoint(lat: Double, lon: Double, index: Int): WeatherPoint
+    suspend fun getGroundWeatherPoint(lat: Double, lon: Double): WeatherPoint
 }
 
 class LocationForecastRepositoryImplementation(
     private val locationforecastDataSource: LocationForecastDataSource = LocationForecastDataSource()
 ) : LocationForecastRepository {
     private val LOG_NAME = "LOCATION_FORECAST_REPOSITORY"
+    private var addDaylightSave = 0
 
     /**
      * Fetches locationforecastData via the dataSource. If exception is thrown, it will return an
@@ -45,11 +46,18 @@ class LocationForecastRepositoryImplementation(
      * @param index the index is used to get the wanted start timeframe
      * @return weatherpoint with ground level information
      */
-    override suspend fun getGroundWeatherPoint(lat: Double, lon: Double, index: Int): WeatherPoint {
+    override suspend fun getGroundWeatherPoint(lat: Double, lon: Double): WeatherPoint {
+        //Compute local time from UTC + timezone + DST.
+        // UTC time for current day is at index 0 - Norwegian time is UTC + 2 (from last sunday in March) during summer and UTC + 1 during winter (from last sunday in October) .
+        // DST changes: Last sunday in March and last sunday in October
+
+        changeDSTBasedOnDate() //resetting addDaylightSave to 1 or 2 based on month
+
+        val dateTimeIndex = addDaylightSave
         val allLocationData = getLocationForecastData(lat, lon)
 
         val timeSeriesInstantDetails: Details? = // Reduces boilerplate later on
-            allLocationData.properties?.timeseries?.getOrNull(index)?.data?.instant?.details
+            allLocationData.properties?.timeseries?.getOrNull(dateTimeIndex)?.data?.instant?.details
 
         val windSpeed: Double? =
             timeSeriesInstantDetails?.wind_speed
@@ -62,10 +70,11 @@ class LocationForecastRepositoryImplementation(
         val cloudFraction: Double? =
             timeSeriesInstantDetails?.cloud_area_fraction
         val rain: Double? =
-            allLocationData.properties?.timeseries?.getOrNull(index)?.data?.next_1_hours?.details?.precipitation_amount
+            allLocationData.properties?.timeseries?.getOrNull(dateTimeIndex)?.data?.next_1_hours?.details?.precipitation_amount
         val relativeHumidity: Double? =
             timeSeriesInstantDetails?.relative_humidity
         val dewPoint: Double = computeDewPointGround(airTemperature, relativeHumidity)
+        val heigth: Double = allLocationData.geometry?.coordinates?.getOrNull(2)!!
 
         //var weatherPoint = GroundWeatherPoint(windSpeed, windFromDirection, airTemperature, pressureSeaLevel,cloudFraction,rain, humidity, 0.0)
         val weatherPoint = WeatherPoint(
@@ -76,12 +85,30 @@ class LocationForecastRepositoryImplementation(
             cloudFraction = cloudFraction!!,
             rain = rain!!,
             humidity = relativeHumidity!!,
-            height = allLocationData.geometry?.coordinates?.getOrNull(2)!!,
+            height = heigth,
             dewPoint = dewPoint
         )
 
         return weatherPoint
     }
+
+    fun changeDSTBasedOnDate() {
+        val calendar: Calendar = Calendar.getInstance()
+        val dayOfWeek: Int =
+            calendar.get(Calendar.DAY_OF_WEEK_IN_MONTH) //keeping here for more detailed computing later
+        val dayOfMonth: Int =
+            calendar.get(Calendar.DAY_OF_MONTH) //keeping here for more detailed computing later
+        val month: Int = calendar.get(Calendar.MONTH)
+
+        // Note: months are 0-based (January is 0)
+        // Change the value of the variable based on the date
+        if (month >= 3 && month <= 10) { //April to (including) October.
+            addDaylightSave = 2
+        } else { // December - March
+            addDaylightSave = 1
+        }
+    }
+
 
 }
 
