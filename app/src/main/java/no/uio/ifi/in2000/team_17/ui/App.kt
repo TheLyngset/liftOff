cd0037dd6f8a49f80bf1f3ec621be5dde54150ab
Malin Kristine Bjørnstadjordet <malinkbj@ifi.uio.no>
2024-04-05 11:26:23 +0200

    Add logo to topBar.

--- app/src/main/java/no/uio/ifi/in2000/team_17/ui/App.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/ui/App.kt
@@ -1,5 +1,7 @@
 package no.uio.ifi.in2000.team_17.ui
 
+import android.graphics.Paint.Align
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +11,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -24,6 +27,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -31,25 +37,26 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.launch
+import no.uio.ifi.in2000.team_17.R
 import java.lang.NumberFormatException
 
 
-enum class Screen(val title: String) {
-    Home(title = "Home Screen"),
-    Input(title = "Input Screen")
+enum class Screen(val title: String, val logo: Int) {
+    Home(title = "Home Screen", logo = R.drawable.logor),
+    Input(title = "Input Screen", logo = R.drawable.logor)
 }
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppTopBar(
     modifier: Modifier = Modifier,
-    currentScreen: Screen
+    currentScreen: Screen,
+    logoId: Int
  ){
     TopAppBar(
-        title = { Text(currentScreen.title) },
+        title = { Image(painter = painterResource(id = logoId), contentDescription = "Test") },
         colors = TopAppBarDefaults.mediumTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer
-        ),
-        modifier = modifier,
+        )
     )
 }
 
@@ -67,7 +74,9 @@ fun App(
     Scaffold(
         topBar = {
             AppTopBar(
-                currentScreen = Screen.Home
+                currentScreen = Screen.Home,
+                logoId = Screen.Home.logo,
+                modifier = Modifier.padding(horizontal = 5.dp)
             )
         },
         bottomBar = {
