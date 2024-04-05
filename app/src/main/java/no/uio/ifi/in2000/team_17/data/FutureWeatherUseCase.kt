e070eea5e5147fdb558c02b276e77632851d3e97
Lelia M <leliamm@ulrik.uio.no>
2024-04-05 20:19:11 +0200

    Update FutureWeatherUseCase.kt
    added methods to retrieve data for the next 6 days. Not tested.

--- app/src/main/java/no/uio/ifi/in2000/team_17/data/FutureWeatherUseCase.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/data/FutureWeatherUseCase.kt
@@ -1,29 +1,36 @@
 package no.uio.ifi.in2000.team_17.data
 
+import kotlinx.coroutines.flow.MutableStateFlow
 import no.uio.ifi.in2000.team_17.model.WeatherPoint
-import no.uio.ifi.in2000.team_17.model.weatherDTO.LocationforecastDTO
 
 //har tilgang til repository og returnerer use case data. F. eks temperatur
 //henter data fra alle repo som trengs.
 // a use case should have only one plublic function. All other functions have to be private utility functions. - single responsibility principle
-class FutureWeatherUseCase(private val repository: Repository) {
-    suspend fun allData(): LocationforecastDTO {
-        return repository.getLocationForecast()
+class FutureWeatherUseCase(
+    private val repository: Repository = RepositoryImplementation(),
+) {
+    suspend fun next24H(): MutableStateFlow<MutableList<MutableStateFlow<List<WeatherPoint>>>> {
+        return repository.getListOfWeatherPointsLists(2, 26)
     }
 
-    suspend fun today(): WeatherPoint? {
-        // TODO:
-        return null
+    suspend fun next48h(): MutableStateFlow<MutableList<MutableStateFlow<List<WeatherPoint>>>> {
+        return repository.getListOfWeatherPointsLists(27, 50)
     }
 
-    suspend fun day2(): WeatherPoint? {
-        // TODO:
-        return null
+    suspend fun next3D(): MutableStateFlow<MutableList<MutableStateFlow<List<WeatherPoint>>>> {
+        return repository.getListOfWeatherPointsLists(51, 75)
     }
 
-    suspend fun day3(): WeatherPoint? {
-        // TODO:
-        return null
+    suspend fun next4D(): MutableStateFlow<MutableList<MutableStateFlow<List<WeatherPoint>>>> {
+        return repository.getListOfWeatherPointsLists(76, 100)
+    }
+
+    suspend fun next5D(): MutableStateFlow<MutableList<MutableStateFlow<List<WeatherPoint>>>> {
+        return repository.getListOfWeatherPointsLists(101, 125)
+    }
+
+    suspend fun next6D(): MutableStateFlow<MutableList<MutableStateFlow<List<WeatherPoint>>>> {
+        return repository.getListOfWeatherPointsLists(126, 150)
     }
 
 
