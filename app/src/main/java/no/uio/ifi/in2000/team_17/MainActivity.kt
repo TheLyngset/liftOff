df6018f9856427188cf8cb572c37d0f16e66d77d
samuelvl <samuelvl@uio.no>
2024-04-12 21:48:35 +0200

    Added Context.dataStore as a val in MainActivity

--- app/src/main/java/no/uio/ifi/in2000/team_17/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/MainActivity.kt
@@ -1,18 +1,28 @@
 package no.uio.ifi.in2000.team_17
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
+import no.uio.ifi.in2000.team17.AdvancedSettings
+import no.uio.ifi.in2000.team_17.data.AdvancedSettingsRepository
+import no.uio.ifi.in2000.team_17.data.AdvancedSettingsSerializer
 import no.uio.ifi.in2000.team_17.data.Repository
 import no.uio.ifi.in2000.team_17.data.RepositoryImplementation
 import no.uio.ifi.in2000.team_17.ui.App
 import no.uio.ifi.in2000.team_17.ui.theme.Team17Theme
 
-
+val Context.dataStore: DataStore<AdvancedSettings> by dataStore(
+    fileName = "advanced_settings",
+    serializer = AdvancedSettingsSerializer
+)
 class MainActivity : ComponentActivity() {
     val repository: RepositoryImplementation by lazy {
         RepositoryImplementation()
@@ -37,3 +47,4 @@ class MainActivity : ComponentActivity() {
         val repository: Repository = RepositoryImplementation()
     }
 }
+
