e35e144bc162acacf45d7c29318986ed9be66cb1
Lelia M. <leliamm@ulrik.uio.no>
2024-04-15 13:37:34 +0200

    added each screen to the enum class Screen

--- app/src/main/java/no/uio/ifi/in2000/team_17/ui/App.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/ui/App.kt
@@ -1,6 +1,5 @@
 package no.uio.ifi.in2000.team_17.ui
 
-import android.graphics.Paint.Align
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -9,11 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -23,14 +18,10 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -38,37 +29,44 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.launch
 import no.uio.ifi.in2000.team_17.App
 import no.uio.ifi.in2000.team_17.R
 import no.uio.ifi.in2000.team_17.ui.advanced_settings.AdvancedSettingsScreen
 import no.uio.ifi.in2000.team_17.ui.advanced_settings.AdvancedSettingsViewModel
-import no.uio.ifi.in2000.team_17.ui.home_screen.HomeScreen
 import no.uio.ifi.in2000.team_17.ui.home_screen.HomeScreenViewModel
 import no.uio.ifi.in2000.team_17.viewModelFactory
-import java.lang.NumberFormatException
 
 
 enum class Screen(val title: String, val logo: Int) {
     Home(title = "Home Screen", logo = R.drawable.logoicon),
-    AdvancedSettings(title = "Advanced Settings", logo = R.drawable.logor)
+    AdvancedSettings(title = "Advanced Settings", logo = R.drawable.logor),
+    ResultsScreen(title = "Results Screen", logo = R.drawable.logor),
+    LawScreen(title = "Law Screen", logo = R.drawable.logor),
+    TechnicalDetailsScreen(title = "Technical Details", logo = R.drawable.logor),
+
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppTopBar(
     modifier: Modifier = Modifier,
     currentScreen: Screen,
     logoId: Int
- ){
+) {
     TopAppBar(
 
         title = {
 
-            Row (modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween){
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
                 Image(painter = painterResource(id = logoId), contentDescription = "Logo")
-                Text(text = "Oslo", modifier = Modifier.padding(horizontal = 8.dp, vertical = 10.dp))
+                Text(
+                    text = "Oslo",
+                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 10.dp)
+                )
                 Image(
                     painter = painterResource(id = R.drawable.search_24px),
                     contentDescription = "Search",
@@ -87,14 +85,18 @@ fun AppTopBar(
 
 @Composable
 fun App(
-    navController:NavHostController = rememberNavController(),
+    navController: NavHostController = rememberNavController(),
 
-){
+    ) {
     //Using viewModel Factories to take the repository created in Main activity as a parameter
     //Todo implemnt the new viewModelFactory for this viewModel
     val homeScreenViewModel: HomeScreenViewModel = viewModel(
         factory = viewModelFactory {
-            HomeScreenViewModel(App.appModule.repository, App.appModule.settingsRepository, App.appModule.advancedSettingsRepository)
+            HomeScreenViewModel(
+                App.appModule.repository,
+                App.appModule.settingsRepository,
+                App.appModule.advancedSettingsRepository
+            )
         }
     )
     val homeScreenUiState by homeScreenViewModel.homeScreenUiState.collectAsState()
@@ -130,19 +132,23 @@ fun App(
             }
         }, */
         snackbarHost = { SnackbarHost(snackbarHostState) }
-    ){innerPadding ->
+    ) { innerPadding ->
         NavHost(
             navController = navController,
             startDestination = Screen.Home.name
-        ){
-            composable(route = Screen.Home.name){
+        ) {
+            composable(route = Screen.Home.name) {
                 newHomeScreen(
                     Modifier
                         .padding(innerPadding)
-                        .verticalScroll(scrollStateVertical))
+                        .verticalScroll(scrollStateVertical)
+                )
             }
-            composable(route = Screen.AdvancedSettings.name){
-                AdvancedSettingsScreen(Modifier.padding(innerPadding),viewModel = advancedSettingsViewModel, Navigate = {navController.navigate(Screen.Home.name)}){
+            composable(route = Screen.AdvancedSettings.name) {
+                AdvancedSettingsScreen(
+                    Modifier.padding(innerPadding),
+                    viewModel = advancedSettingsViewModel,
+                    Navigate = { navController.navigate(Screen.Home.name) }) {
                     coroutineScope.launch {
                         snackbarHostState.showSnackbar(
                             message = "Invalid input"
