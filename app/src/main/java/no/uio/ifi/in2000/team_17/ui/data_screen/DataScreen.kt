b883ce9379ef2bbc6339945ef6c52d24e6f967cd
Lelia M. <leliamm@ulrik.uio.no>
2024-04-17 23:45:30 +0200

    update dataScreen table with missing values and with function to show the graphchart

--- app/src/main/java/no/uio/ifi/in2000/team_17/ui/data_screen/DataScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/ui/data_screen/DataScreen.kt
@@ -1,15 +1,9 @@
 package no.uio.ifi.in2000.team_17.ui.data_screen
 
 import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -23,58 +17,57 @@ import androidx.compose.ui.unit.dp
 fun DataScreen(
     modifier: Modifier = Modifier,
     dataScreenUiState: DataScreenUiState
-){
+) {
     val state = rememberScrollState()
     LaunchedEffect(Unit) { state.animateScrollTo(50) }
-
-
-        Column {
-
-
-            Row(
-                modifier
-
-
-            ) {
-                Column(modifier.padding(3.dp)) {
-                    Text(text = "Time:")
-                    Text(text = "GroundWind:")
-                    Text("Max Wind Shear:")
-                    Text("Cloud fraction:")
-                    Text("Median rain:")
-                    Text("Humidity:")
-                    Text("Dew point")
-                    Text("Fog fraction")
-                }
-                Row(
-                    Modifier
-
-                        .horizontalScroll(state)
-                ) {
-                    for (index: Int in 0..5) {
-                        val weatherPointInTime = dataScreenUiState.weatherDataLists.get(index)
-                        Column(modifier.padding(3.dp)) {
-                            Text(text = weatherPointInTime.time)
-                            Text(text = weatherPointInTime.groundWind.speed.toString())
-                            Text(weatherPointInTime.maxWindShear.speed.toString())
-                            Text(weatherPointInTime.cloudFraction.toString())
-                            Text(weatherPointInTime.rain.median.toString())
-                            Text(weatherPointInTime.humidity.toString())
-                            Text(weatherPointInTime.dewPoint.toString())
-                            Text(weatherPointInTime.fog.toString())
-                        }
-                    }
-                }
+    Column {
+        Row() {
+            Column(modifier.padding(3.dp)) {
+                Text(text = "Time:")
+                Text(text = "Ground Wind:")
+                Text("Max Wind Shear:")
+                Text("Cloud fraction:")
+                Text("Median rain:")
+                Text("Humidity:")
+                Text("Dew point")
+                Text("Fog fraction")
+                Text("Max Wind")
             }
-            Spacer(Modifier.size(20.dp))
             Row(
                 Modifier
 
+                    .horizontalScroll(state)
             ) {
+                for (index: Int in 0..70) {
+                    val weatherPointInTime = dataScreenUiState.weatherDataLists.get(index)
+                    Column(modifier.padding(3.dp)) {
+                        Text(text = weatherPointInTime.time)
+                        Text(text = weatherPointInTime.groundWind.speed.toString())
+                        Text(weatherPointInTime.maxWindShear.speed.toString())
+                        Text(weatherPointInTime.cloudFraction.toString())
+                        Text(weatherPointInTime.rain.median.toString())
+                        Text(weatherPointInTime.humidity.toString())
+                        Text(weatherPointInTime.dewPoint.toString())
+                        Text(weatherPointInTime.fog.toString())
+                        Text(weatherPointInTime.maxWind.speed.toString())
 
-                pre()
-
+                    }
+                }
             }
         }
+        Spacer(Modifier.size(20.dp))
+        Row(
+            Modifier
+
+        ) {
+//            pre()
+            /// LinesGraph(dataScreenUiState.weatherDataLists)
+            ThresholdGraph(dataScreenUiState.weatherDataLists, dataScreenUiState.advancedSettings)
+
+        }
+        Row {
+            ChartHistory()
+        }
+    }
 
 }
