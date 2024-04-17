3acb599fe1be07c995bbe5104ff295c26a624287
Lelia M. <leliamm@ulrik.uio.no>
2024-04-17 23:45:04 +0200

    Update thresholds to avoid division by 0

--- app/src/main/java/no/uio/ifi/in2000/team_17/model/Thresholds.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/model/Thresholds.kt
@@ -1,12 +1,12 @@
 package no.uio.ifi.in2000.team_17.model;
 
 data class Thresholds(
-    val windSpeed: Double = 8.6,
+    val groundWindSpeed: Double = 8.6,
     val maxWindSpeed: Double = 17.2,
     val maxWindShear: Double = 24.5,
     val cloudFraction: Double = 15.0,
-    val fog: Double = 0.0,
-    val rain: Double = 0.0,
+    val fog: Double = 0.01,
+    val rain: Double = 0.01,
     val humidity: Double = 75.0,
     val dewPoint: Double = 15.0,
     val margin: Double = 0.6
