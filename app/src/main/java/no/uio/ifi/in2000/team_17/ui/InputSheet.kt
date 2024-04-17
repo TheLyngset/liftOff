56d065b721095a7b5c3bbcf4967f0d62bfa6aca6
samuelvl <samuelvl@uio.no>
2024-04-17 22:53:02 +0200

    minor bugfixes

--- app/src/main/java/no/uio/ifi/in2000/team_17/ui/InputSheet.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/ui/InputSheet.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
@@ -86,6 +87,7 @@ fun InputSheetContent(
             verticalAlignment = Alignment.CenterVertically) 
         {
             InputTextField(
+                modifier = Modifier.fillMaxWidth(),
                 value = maxHeightText,
                 onValueChange = { maxHeightText = it },
                 label = "Maximum height in km"
