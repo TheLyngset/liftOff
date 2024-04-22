cc4531e938e460669cf8e963599e6be254571ee2
Malin Kristine Bjørnstadjordet <malinkbj@ifi.uio.no>
2024-04-22 14:19:38 +0200

    Added background to JudicialScreen

--- app/src/main/java/no/uio/ifi/in2000/team_17/ui/judicial_screen/JudicialScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/ui/judicial_screen/JudicialScreen.kt
@@ -7,15 +7,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import no.uio.ifi.in2000.team_17.ui.BackGroundImage
+import no.uio.ifi.in2000.team_17.ui.data_screen.TitleBox
 
 @Composable
 fun JudicialScreen(modifier: Modifier) {
+    BackGroundImage(alpha = 0.75f)
     Column(
         modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text("This is the Judicial Screen")
+        TitleBox(text = "Judicial Screen")
         Text(
             "Juridisk fra Håkon: \nKart over luftområder: https://www.ippc.no/ippc/index.jsp\n" +
                     "Api'et til kartet: https://ais.avinor.no/no/AIP/\n" +
