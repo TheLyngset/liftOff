13dcf71416fbd587314c61e5f9873b0cef2b97df
samuelvl <samuelvl@uio.no>
2024-04-24 14:25:40 +0200

    Resolved mergeconflict

--- app/src/main/java/no/uio/ifi/in2000/team_17/ui/home_screen/HomeScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/ui/home_screen/HomeScreen.kt
@@ -21,7 +21,7 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
-Meimport androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
