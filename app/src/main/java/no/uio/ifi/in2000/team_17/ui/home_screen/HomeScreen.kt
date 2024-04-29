d24295e09461852069f99c39240a9787643cb3d6
Sander <Sandern@uio.no>
2024-04-29 14:42:18 +0200

    Centered rocket with offset

--- app/src/main/java/no/uio/ifi/in2000/team_17/ui/home_screen/HomeScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/ui/home_screen/HomeScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyListState
@@ -78,7 +79,8 @@ fun HomeScreen(
     }
 
     if(windowSizeClass.heightSizeClass != WindowHeightSizeClass.Compact) {
-        Box(modifier){
+        Box(modifier = Modifier.fillMaxWidth().offset(y= (100.dp)),
+            contentAlignment = Alignment.Center){
             Rocket()
         }
 
