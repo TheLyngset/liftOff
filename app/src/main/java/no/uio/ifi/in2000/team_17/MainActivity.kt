3ec318494a6dd380bca4c93398fc8cd1f1440b06
samuelvl <samuelvl@uio.no>
2024-04-30 23:17:15 +0200

    Added image to ARCHITECTURE.md

--- app/src/main/java/no/uio/ifi/in2000/team_17/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_17/MainActivity.kt
@@ -78,7 +78,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.primaryContainer
                 ) {
-                    if (false) {
+                    if (!uiState.hasData) {
                         val context = LocalContext.current
                         NoDataScreen(viewModel = splashScreenViewModel) {
                             context.restart()
